f52630f3d0087c06565e5d30d5e4d26d8be267e2
JiaLee0707 <s2018s37@e-mirim.hs.kr>
2019-12-01 18:39:46 +0900

    add obnoxious BurgerRandom

---
+++ src/BurgerMi__77/Timer.java
@@ -0,0 +1,37 @@
+package BurgerMi__77;
+
+import java.awt.Font;
+
+import javax.swing.JLabel;
+
+public class Timer extends Thread {
+	int[] Time = { 1, 5 };
+
+	public JLabel time = new JLabel(Time[0] + ":" + Time[1]);
+
+	Timer() {
+		time.setFont(new Font("나눔스퀘어라운드 ExtraBold", Font.ITALIC, 50)); // 폰트 설정
+		time.setSize(500, 500); // 크키 설정
+		time.setLocation(1140, -200); // 위치 설정
+		
+//		BurgerMi.game.add(time);
+		this.start();
+	}
+
+	public void run() {
+		try {
+			while(true) {
+				Time[1]-=1;					
+				time.setText(Time[0]+":"+Time[1]);
+				if(Time[1]==0) {
+					if(Time[0]==0) break;
+					Time[0]-=1;
+					Time[1]=60;
+				}
+				Thread.sleep(1000);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
