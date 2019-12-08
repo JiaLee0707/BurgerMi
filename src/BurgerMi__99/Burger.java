f3d79aa5182b90263b50d3603b87c07f494454e9
JiaLee0707 <s2018s37@e-mirim.hs.kr>
2019-12-08 12:41:18 +0900

    소스코드 정리(+주석추가)

--- src/BurgerMi__99/Burger.java
+++ src/BurgerMi__99/Burger.java
@@ -6,12 +6,12 @@ import java.util.List;
 
 //햄버거 내려오는 쓰레드
 public class Burger extends Thread {
-	int x, y;
-	String StringIngeredients;
-	Image ingredients;
+	int x, y; // 햄버거 x,y 좌표
+	String StringIngeredients; // 햄버거 재료 이름
+	Image ingredients; // 햄버거 재료 이미지
 
+	// 햄버거 재료, 좌표 초기화
 	Burger(int x, int y, Image ingredients, String string) {
-		// 햄버거 재료, 좌표 초기화
 		this.x = x;
 		this.y = y;
 		this.ingredients = ingredients;
@@ -20,14 +20,15 @@ public class Burger extends Thread {
 
 	public void run() {
 		int i = 0;
-		boolean bool = true;
+		boolean bool = true; // 햄버거 내려오냐/안내려오냐
 		try {
 			// i가 y가 아닐때까지 스레드 실행
 			while (i != y) {
 				i = i + 1;
-				BurgerMi.game.Thread(ingredients, x, i, bool);
-				BurgerMi.game.repaint();
-				Thread.sleep((long) 0.9);
+				BurgerMi.game.Thread(ingredients, x, i, bool); // 화면에 그리기 위해
+																// Game 클래스에 변수 초기화
+				BurgerMi.game.repaint(); // 화면 다시그리기
+				Thread.sleep(1); // 햄버거 재료 내려오는 속도
 			}
 
 			// MakeBurger에 추가
@@ -36,9 +37,10 @@ public class Burger extends Thread {
 			BurgerArray.add(this.ingredients);
 			BurgerArray.add(this.x);
 			BurgerArray.add(this.y);
+			// 전체 햄버거에 재료 추가
 			BurgerMi.game.order.MakeBurger.add((List<Object>) BurgerArray);
 			System.out.println("");
-			bool = false;
+			bool = false; // 햄버거 내려오기 끝
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
