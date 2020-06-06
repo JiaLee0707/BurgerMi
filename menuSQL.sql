c1d10b1d9c6ecd3d29d80eeed7f538a882a14fc7
JiaLee0707 <s2018s37@e-mirim.hs.kr>
2020-06-06 23:16:12 +0900

    DB table 준비 중

--- menuSQL.sql
+++ menuSQL.sql
@@ -3,5 +3,9 @@ drop TABLE menu;
 
 CREATE TABLE menu (NAME VARCHAR(20) NOT NULL PRIMARY key, ingredients VARCHAR(20), price INT(5) NOT NULL, sort VARCHAR(10) not NULL);
 
-INSERT INTO menu ("
-
+INSERT INTO menu ("디폴트 버거", "", 2000, "단품"),
+					  ("함박 버거", "", 2000, "단품"),
+					  ("치즈 버거", "", 2000,  "단품"),
+					  ("불고기 버거","", 2000, "단품"),
+					  ("더블치즈 버거", "" 4500, "단품"),
+					  ("");,
--- recipesSQL.sql
+++ recipesSQL.sql
@@ -2,3 +2,9 @@ USE burgermi;
 DROP TABLE recipes;
 
 CREATE TABLE recipes ( NAME VARCHAR(20) NOT NULL, ingredient VARCHAR(50) NOT NULL);
+
+INSERT INTO menu ("디폴트 버거", "아래빵;패티;양파;윗빵"),
+					  ("함박 버거", "아래빵;패티;치즈;윗빵"),
+					  ("치즈 버거", "아래빵;패티;치즈;토마토;윗빵"),
+					  ("불고기 버거","아래빵;패티;양상추;양파;윗빵"),
+					  ("더블치즈 버거", "아래빵;패티;치즈;패티;치즈;양상추;윗빵");
